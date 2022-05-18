c2f4728bd9ff76031ecd49d6e3ff70b437e36c42
Luis Fernando Tapia <88354259+Luisf718@users.noreply.github.com>
2022-05-17 20:11:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,14 +18,34 @@ def run_query(query):
     with connection.cursor() as cursor:
         cursor.execute(query)
         results = cursor.fetchall()
+        df = pd.read_sql_query(select_query,connection)
         cursor.close()
         connection.close()
         return results
+ 
+rows = run_query('''SELECT *
+FROM PUBLIC.accommodations a
+JOIN PUBLIC.cities c ON c.id = a.id_city
+ORDER BY a.id;''')
 
-rows = run_query(''' SELECT * 
-  FROM PUBLIC.landlords
-  WHERE id > 500;''')
+#Agrupamos por el nombre de las ciudades y sumamos las visitas que han tenido por toda la ciudad
+df_groupby_ciudad_visitas = df.groupby(by='name')['number_of_visits'].agg([sum, min, max])
+#Reseteamos los index para que 'name' se ponga como columna y no se quede en indice
+df_groupby_ciudad_visitas = df_groupby_ciudad_visitas.reset_index()
+#Ordenamos el df por el 'sum' para que esten ordenados del que tiene mas visitas al que tiene menos
+df_groupby_ciudad_visitas = df_groupby_ciudad_visitas.sort_values('sum', ascending=False)
 
-# Print results.
-for row in rows:
-    st.write(row)
+x = df_groupby_ciudad_visitas['name'][:5]
+y = df_groupby_ciudad_visitas['sum'][:5]
+plt.bar(x, y, color='red')
+plt.xlabel('Ciudad')
+plt.ylabel('Visitas')
+plt.title('TOP 5 visitas por ciudad')
+st.write("Hola mundo")
+st.write(plt.show())
+
+
+
+# # Print results.
+# for row in rows:
+#     st.write(row)
