23b629f52c61a7730a69ffd65242412e88646009
Luis Fernando Tapia <88354259+Luisf718@users.noreply.github.com>
2022-05-17 20:30:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 def init_connection():
     return psycopg2.connect(**st.secrets["postgres"])
 
-# connection = init_connection()
+connection = init_connection()
 
 # Perform query.
 # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
@@ -25,6 +25,34 @@ def run_query(query):
         cursor.close()
         connection.close()
         return df
+
+try:
+  connection1 = connection
+  #Creamos el cursor para las operaciones de la base de datos
+  cursor = connection1.cursor()
+  #Creamos una variable con el codigo sql que queremos que se ejecute
+  select_query = '''SELECT *
+FROM PUBLIC.accommodations a
+JOIN PUBLIC.cities c ON c.id = a.id_city
+ORDER BY a.id;'''
+  #Executamos el comando
+  cursor.execute(select_query)
+  connection1.commit()
+  #con la funcion fetchall() podemos ver lo que retornaria la base de datos
+  #df_accommodations = cursor.fetchall()
+  #print(df_accommodations)
+  #Esto crea un data frame con la información que pediste de la base de datos
+  df = pd.read_sql_query(select_query,connection1)
+  # print("Result ", cursor.fetchall())
+
+#Por si la conexion no fue exitosa
+except (Exception, Error) as error:
+  print("Error while connecting to PostgreSQL", error)
+finally:
+  if (connection):
+    cursor.close()
+    connection.close()
+    print("PostgreSQL connection is closed")
  
 df = run_query("SELECT * FROM PUBLIC.accommodations a JOIN PUBLIC.cities c ON c.id = a.id_city ORDER BY a.id;")
 
