453a9bf922fd169404b55c36f6908a0caeb25c5b
Luis Fernando Tapia <88354259+Luisf718@users.noreply.github.com>
2022-05-18 18:50:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,23 +24,23 @@ conn = init_connection()
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
-#         df = pd.read_sql_query(query,conn)
+        df = pd.read_sql_query(query,conn)
         return cur.fetchall()
 
-rows = run_query("SELECT * from PUBLIC.accommodations;")
+# rows = run_query("SELECT * from PUBLIC.accommodations;")
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
-
-
-
-# df = run_query("SELECT * FROM PUBLIC.accommodations a JOIN PUBLIC.cities c ON c.id = a.id_city ORDER BY a.id;")
+# # Print results.
+# for row in rows:
+#     st.write(f"{row[0]} has a :{row[1]}:")
 
 
 
-# st.subheader('Raw data')
-# st.write(df)
+df = run_query("SELECT * FROM PUBLIC.accommodations a JOIN PUBLIC.cities c ON c.id = a.id_city ORDER BY a.id;")
+
+
+
+st.subheader('Raw data')
+st.write(df)
 
 
 
