78c3de57b8e09608a6e8254ed027070fca5ec456
Luis Fernando Tapia <88354259+Luisf718@users.noreply.github.com>
2022-05-18 21:27:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,9 +62,10 @@ y = df_groupby_ciudad_visitas['sum'][:5]
 plt.bar(x, y, color='red')
 plt.xlabel('Ciudad')
 plt.ylabel('Visitas')
-# fig = plt.show()
-png = plt.savefig("Ejemplo1.jpg")
+fig = plt.show()
+st.pyplot(fig)
+
 number_of_visits_char = plt.figure()
 st.subheader('Top 5 Numero de visitas')
-st.pyplot(png)
-# st.bar_chart(number_of_visits_char)
+
+st.bar_chart(number_of_visits_char)
