e507e09df91cdd0cb83d81d4377bf3cfb573bb9d
Luis Fernando Tapia <88354259+Luisf718@users.noreply.github.com>
2022-05-27 11:01:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,35 +7,31 @@ import matplotlib.pyplot as plt
 from psycopg2 import Error
 import numpy as np
 
-
-connection = psycopg2.connect(user="vyzgmpqsxeucnv",
+def conection_sql(query:str):
+  connection = psycopg2.connect(user="vyzgmpqsxeucnv",
                             password="480540f32aa53c6f6850fee0add13f0ae8211a9aa7c98ed18fab701a829869df",
                             host="ec2-54-157-79-121.compute-1.amazonaws.com",
                             port="5432",
                             database="d1evcvc2sccml6")
-#Creamos el cursor para las operaciones de la base de datos
-cursor = connection.cursor()
-#Creamos una variable con el codigo sql que queremos que se ejecute
-select_query = '''SELECT *
-FROM PUBLIC.accommodations a
-JOIN PUBLIC.cities c ON c.city_id = a.id_city
-ORDER BY a.id;'''
-#Executamos el comando
-cursor.execute(select_query)
-connection.commit()
-#con la funcion fetchall() podemos ver lo que retornaria la base de datos
-#df_accommodations = cursor.fetchall()
-#print(df_accommodations)
-#Esto crea un data frame con la información que pediste de la base de datos
-df = pd.read_sql_query(select_query,connection)
-# st.write("Result: ", cursor.fetchall())
-
-
-if (connection):
+  #Creamos el cursor para las operaciones de la base de datos
+  cursor = connection.cursor()
+  #Creamos una variable con el codigo sql que queremos que se ejecute
+  select_query = query
+  #Executamos el comando
+  cursor.execute(select_query)
+  connection.commit()
+  #Esto crea un data frame con la información que pediste de la base de datos
+  df = pd.read_sql_query(select_query,connection)
+  if (connection):
   cursor.close()
   connection.close()
   print("PostgreSQL connection is closed")
+  return df
 
+df = conection_sql('''SELECT *
+  FROM PUBLIC.accommodations a
+  JOIN PUBLIC.cities c ON c.city_id = a.id_city
+  ORDER BY a.id;''')
 
 
 #'''Este es el codigo que se utiliza para la primer visualización, Top 5 Numero de visitas por ciudad ''', con estas comillas se puede imprimir 
